6a0c9eb83bb46959bd1b68f449d0545edeca29e0
Keito-luna <aliahmedzannata.jitonline2@gmail.com>
2025-07-30 12:04:25 +0200

    Delete monsite directory

--- monsite/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    var texteSuiveur = document.getElementById('blocSuiveur');
-
-    document.addEventListener('mousemove', function(e) {
-        texteSuiveur.style.left = (e.pageX + 10) + 'px';
-        texteSuiveur.style.top = (e.pageY + 10) + 'px';
-    });
-});
